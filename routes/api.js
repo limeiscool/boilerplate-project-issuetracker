878339b2f3b0cfd40d6943295458f56314da5c3f
Richard Marshall <rich.qazwsx@gmail.com>
2024-02-19 20:24:31 -0800

    fix: title/text now recieved

--- routes/api.js
+++ routes/api.js
@@ -14,18 +14,31 @@ module.exports = function (app) {
 
     .get(function (req, res) {
       let project = req.params.project;
+      let ProjObj = Project.findOne({ project_name: project });
+      ProjObj.then(async (data) => {
+        if (data == null) {
+          const newProject = new Project({
+            project_name: project,
+            issues: [],
+          });
+          await newProject.save();
+          res.json(newProject.issues);
+        } else {
+          res.json(data.issues);
+        }
+      });
     })
 
     .post(async (req, res) => {
       let project = req.params.project;
       let issue = {
-        title: req.body.title,
-        text: req.body.text,
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
         created_on: dateAutoGen(),
         updated_on: dateAutoGen(),
         created_by: req.body.created_by,
-        assigned_to: req.body.assigned_to,
-        status_text: req.body.status_text,
+        assigned_to: req.body.assigned_to || "",
+        status_text: req.body.status_text || "",
         open: true,
       };
       await Project.updateOne(
@@ -46,8 +59,18 @@ module.exports = function (app) {
       });
     })
 
-    .put(function (req, res) {
+    .put(async (req, res) => {
       let project = req.params.project;
+      const currentProject = await Project.findOne({
+        project_name: project,
+      });
+
+      console.log(currentProject);
+      // const updatedProject = await Project.findOneAndUpdate(
+      //   { project_name: project, "issues._id": id },
+      //   { $set: { "issues.$": updatedIssue } },
+      //   { new: true }
+      // )
     })
 
     .delete(function (req, res) {
